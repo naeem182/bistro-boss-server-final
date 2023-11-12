77749c08b378caaacc8a0893a6da1b3f605ace89
naeem <naeemislamjr182@gmail.com>
2023-11-12 21:47:54 +0600

    get menu and review data

--- index.js
+++ index.js
@@ -22,6 +22,31 @@ const client = new MongoClient(uri);
 async function run() {
     try {
         await client.connect();
+
+
+        const menuCollection = client.db("bistroDB").collection("menu");
+        const reviewCollection = client.db("bistroDB").collection("reviews");
+
+        app.get('/menu', async (req, res) => {
+            const result = await menuCollection.find().toArray();
+            res.send(result);
+        })
+
+        app.get('/reviews', async (req, res) => {
+            const result = await reviewCollection.find().toArray();
+            res.send(result);
+        })
+
+
+
+
+
+
+
+
+
+
+
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
