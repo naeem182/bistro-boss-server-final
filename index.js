143fe8659bf025a2050ba2c182be41dcf73522b3
naeem <naeemislamjr182@gmail.com>
2023-11-17 20:14:47 +0600

    jwt verifyadmin

--- index.js
+++ index.js
@@ -65,6 +65,20 @@ async function run() {
                 next();
             })
         }
+
+        // use verify admin after verifyToken
+        const verifyAdmin = async (req, res, next) => {
+            const email = req.decoded.email;
+            const query = { email: email };
+            const user = await userCollection.findOne(query);
+            const isAdmin = user?.role === 'admin';
+            if (!isAdmin) {
+                return res.status(403).send({ message: 'forbidden access' });
+            }
+            next();
+        }
+
+
         // users related api
         app.post('/users', async (req, res) => {
             const user = req.body;
@@ -79,7 +93,7 @@ async function run() {
             res.send(result);
         })
 
-        app.get('/users', verifyToken, async (req, res) => {
+        app.get('/users', verifyToken, verifyAdmin, async (req, res) => {
 
             const result = await userCollection.find().toArray();
             res.send(result);
